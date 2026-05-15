f687b2ae55b8335c44046e6123aa314834b9db1d
bel-n <nade.belovinova@gmail.com>
2026-05-15 14:16:24 +0200

    encoder added to main: encode and write compressed .huff output file

--- main.c
+++ main.c
@@ -5,12 +5,15 @@
 #include "frequency.h"
 #include "huffman_tree.h"
 #include "code_table.h"
+#include "encoder.h"
 
 #define WAV_FILE "Kanye West - Cant Tell Me Nothing.wav"
+#define OUT_FILE "output.huff"
 
 // gcc -Wall -o huffman main.c buffer.c searchdata.c frequency.c huffman_tree.c code_table.c
 
 int main(void) {
+
     printf("[ Step 1 ] Reading WAV file: %s\n", WAV_FILE);
     FileBuffer wav = readFile(WAV_FILE);
     if (!wav.data) {
@@ -25,13 +28,20 @@ int main(void) {
         return 1;
     }
 
+    unsigned char *audioData = wav.data + dc.offset;
+    long audioSize = (long)dc.size;
+
+    FileBuffer audioBuffer;
+    audioBuffer.data = audioData;
+    audioBuffer.size = audioSize;
+
+
     printf("  WAV file size : %ld bytes\n", wav.size);
     printf("  Audio data at : byte %ld\n", dc.offset);
     printf("  Audio size    : %u bytes\n", dc.size);
 
     printf("\n[ Step 2 ] Counting the byte frequencies...\n");
-    unsigned char *audioData = wav.data + dc.offset;
-    long audioSize = (long)dc.size;
+
 
     FreqTable freq;
     buildFrequencyTable(audioData, audioSize, freq);
@@ -66,10 +76,24 @@ int main(void) {
                table[i].code,
                table[i].length);
     }
-}
+
+
+
+    }
+
+    printf("[ Step 5 ] Encoding and writing '%s'...\n", OUT_FILE);
+
+    long huffSize = encodeToFile(&audioBuffer, table, OUT_FILE);
+    if (huffSize == 0) {
+        fprintf(stderr, "Encoding failed. Exiting.\n");
+        freeHuffmanTree(root);
+        freeFileBuffer(&wav);
+        return 1;
+    }
+    printf("  Output file size: %ld bytes (%.2f MB)\n\n", huffSize, (double)huffSize / (1024.0 * 1024.0));
+
 
     freeHuffmanTree(root);
-
     freeFileBuffer(&wav);
     return 0;
 }
